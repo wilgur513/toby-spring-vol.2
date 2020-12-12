152cd082129d20e39c4abd9bb8b0e46edbc401cf
wilgur513 <wilgur513@naver.com>
2020-12-12 23:03:26 +0900

    제거 메서드 학습(DisposalBean, destroy-method, @PreDestroy, @Bean(destoryMethod))

--- src/test/java/chapter1/InitializeMethodTest.java
+++ src/test/java/chapter1/InitializeMethodTest.java
@@ -1,65 +1,92 @@
 package chapter1;
 
+import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class InitializeMethodTest {
-    @Test
-    public void useInitializeBeanInterface(){
-        ApplicationContext ac = new AnnotationConfigApplicationContext(MyClass1.class);
-        checkIsWellInitialize(ac);
+    static String log;
+
+    @Before
+    public void setUp() throws Exception {
+        log = "";
     }
 
     @Test
-    public void useInitMethod() {
-        ApplicationContext ac = new GenericXmlApplicationContext("/chapter1/initializeMethod.xml");
-        checkIsWellInitialize(ac);
+    public void useInterfaceToCheckWellInitAndDestroy(){
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(MyClass1.class);
+        checkIsWellInitializeAndDestroy(ac);
     }
 
     @Test
-    public void usePostConstructor() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(MyClass3.class);
-        checkIsWellInitialize(ac);
+    public void useXmlToSetInitAndDestroyMethod() {
+        ConfigurableApplicationContext ac = new GenericXmlApplicationContext("/chapter1/initializeMethod.xml");
+        checkIsWellInitializeAndDestroy(ac);
+    }
+
+    @Test
+    public void usePostConstructorAndPreDestroy() {
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(MyClass3.class);
+        checkIsWellInitializeAndDestroy(ac);
     }
 
     @Test
     public void useBeanAnnotationSetInitMethod() {
-        ApplicationContext ac = new AnnotationConfigApplicationContext(MyConfig.class);
-        checkIsWellInitialize(ac);
+        ConfigurableApplicationContext ac = new AnnotationConfigApplicationContext(MyConfig.class);
+        checkIsWellInitializeAndDestroy(ac);
     }
 
-    private void checkIsWellInitialize(ApplicationContext ac){
-        MyClass cls = ac.getBean(MyClass.class);
-        assertThat(cls.log, is("constructor initialize"));
+    private void checkIsWellInitializeAndDestroy(ConfigurableApplicationContext ac){
+        checkIsWellInitialize();
+        ac.close();
+        checkIsWellDestroy();
+    }
+
+    private void checkIsWellInitialize(){
+        assertThat(log, is("constructor initialize"));
+    }
+
+    private void checkIsWellDestroy() {
+        assertThat(log, is("constructor initialize destroy"));
     }
 
     static class MyClass{
-        String log;
-
         public MyClass(){
             log = "constructor";
         }
     }
 
-    static class MyClass1 extends MyClass implements InitializingBean {
+    static class MyClass1 extends MyClass implements InitializingBean, DisposableBean {
         @Override
         public void afterPropertiesSet() throws Exception {
             log += " initialize";
+        }
+
+        @Override
+        public void destroy() throws Exception {
+            log += " destroy";
         }
     }
 
     static class MyClass2 extends MyClass{
         public void initialize(){
             log += " initialize";
+        }
+
+        public void destroy(){
+            log += " destroy";
         }
     }
 
@@ -68,21 +95,27 @@ public class InitializeMethodTest {
         public void initialize(){
             log += " initialize";
         }
+
+        @PreDestroy
+        public void destroy(){
+            log += " destroy";
+        }
     }
 
     static class MyConfig{
-        @Bean(initMethod = "initialize")
+        @Bean(initMethod = "initialize", destroyMethod = "destroy")
         public MyClass4 myClass(){
             return new MyClass4();
         }
-
     }
 
     static class MyClass4 extends MyClass{
         public void initialize(){
             log += " initialize";
         }
+
+        public void destroy(){
+            log += " destroy";
+        }
     }
-
-
 }
