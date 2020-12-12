1dce1afe439f5ccf01293cd99526af3e635285ee
wilgur513 <wilgur513@naver.com>
2020-12-12 22:45:34 +0900

    @PostConstructor 학습

--- src/test/java/chapter1/InitializeMethodTest.java
+++ src/test/java/chapter1/InitializeMethodTest.java
@@ -6,6 +6,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
+import javax.annotation.PostConstruct;
+
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -24,26 +26,38 @@ public class InitializeMethodTest {
         assertThat(cls.log, is("constructor initialize"));
     }
 
-    static class MyClass1 implements InitializingBean {
+    @Test
+    public void usePostConstructor() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(MyClass3.class);
+        MyClass3 cls = ac.getBean(MyClass3.class);
+        assertThat(cls.log, is("constructor initialize"));
+    }
+
+    
+
+    static class MyClass{
         String log;
 
-        public MyClass1() {
+        public MyClass(){
             log = "constructor";
         }
+    }
 
+    static class MyClass1 extends MyClass implements InitializingBean {
         @Override
         public void afterPropertiesSet() throws Exception {
             log += " initialize";
         }
     }
 
-    static class MyClass2{
-        String log;
-
-        public MyClass2() {
-            log = "constructor";
+    static class MyClass2 extends MyClass{
+        public void initialize(){
+            log += " initialize";
         }
+    }
 
+    static class MyClass3 extends MyClass{
+        @PostConstruct
         public void initialize(){
             log += " initialize";
         }
