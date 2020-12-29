f1a969307d80b7d47c4ac44ffe6fd66bd3ab63e9
wilgur513 <wilgur513@naver.com>
2020-12-29 21:10:48 +0900

    @Trasactional을 이용한 트랜잭션 적용

--- src/main/java/chapter2/MemberDao.java
+++ src/main/java/chapter2/MemberDao.java
@@ -1,13 +1,20 @@
 package chapter2;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 import java.util.Map;
 
+@Transactional
 public interface MemberDao {
     void insert(Member m);
     void insertAll(List<Member> members);
     void deleteAll();
+
+    @Transactional(readOnly = true)
     List<Member> findMembers();
+    @Transactional(readOnly = true)
     Member findMemberById(int id);
+    @Transactional(readOnly = true)
     Map<Integer, Member> findMembersForMap();
 }
