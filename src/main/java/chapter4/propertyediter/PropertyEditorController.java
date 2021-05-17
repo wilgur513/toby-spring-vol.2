f1be6d3e8816f9fbf6e03a6e72ccf8b55fa5e350
wilgur513 <wilgur513@naver.com>
2021-05-17 21:30:02 +0900

    addFormatters 메서드를 사용한 Converter 추가

--- src/main/java/chapter4/propertyediter/PropertyEditorController.java
+++ src/main/java/chapter4/propertyediter/PropertyEditorController.java
@@ -2,18 +2,26 @@ package chapter4.propertyediter;
 
 import org.springframework.context.support.ConversionServiceFactoryBean;
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistrar;
+import org.springframework.format.FormatterRegistry;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Controller
-public class PropertyEditorController {
+public class PropertyEditorController extends WebMvcConfigurerAdapter {
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        registry.addConverter(new LevelToStringConverter());
+        registry.addConverter(new StringToLevelConverter());
+    }
+
     @InitBinder
     public void initBinder(WebDataBinder binder) {
-        binder.registerCustomEditor(Level.class, new LevelPropertyEditor());
         binder.registerCustomEditor(Integer.class, "age", new MinMaxPropertyEditor());
     }
 
